edb214df8c48f81c33a86be8858e3b890803a1ec
Nikita Stepanov <75443760+vaprvsapr@users.noreply.github.com>
2024-09-10 14:36:20 +0300

    Update WinMain.cpp

--- 2D_Gas_Sim/WinMain.cpp
+++ 2D_Gas_Sim/WinMain.cpp
@@ -1,10 +1,9 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "System.h"
 int main()
 {
-	System system(300);
-	system.GenerateGridObjects({ 50, 50 }, { 800, 350 }, 100, 1, 0.01, 1);
-	system.GenerateGridObjects({ 50, 500 }, { 800, 350 }, 100, 2, 0.01, 1);
+	System system(1);
+	system.GenerateGridObjects({ 50, 50 }, { 800, 800 }, 2, 1, 100, 0.1);
 	system.SetNumberOfVisualizedObjects(1000);
 	system.SetPistolState(false);
 	system.Run();
